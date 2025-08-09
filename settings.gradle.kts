47b79825d845deef41968eca65aeaf2097668beb
3441507118 <84115720+3441507118@users.noreply.github.com>
2025-08-09 19:29:53 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -47,8 +47,7 @@ val minecraftVersion: String by settings
 rootProject.name = "jei-${minecraftVersion}"
 include(
 	"Core",
-	"Changelog",
-	"Common", "CommonApi",
+	"Changelog",,
 	"Fabric", "FabricApi",
 	"Library",
 	"Gui"
